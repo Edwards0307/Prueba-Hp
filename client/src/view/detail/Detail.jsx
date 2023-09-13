32d88a6b542e5cd1f1b4987c559c6be391965374
Edwards0307 <edwards.ardila03@gmail.com>
2023-09-13 11:07:46 -0500

    Rutas definidas

---
+++ client/src/view/detail/Detail.jsx
@@ -0,0 +1,9 @@
+const Detail = () => {
+    return (
+        <div>
+            <h1>Estoy en detail</h1>
+        </div>
+    )
+};
+
+export default Detail;
---
+++ client/src/view/form/Form.jsx
@@ -0,0 +1,9 @@
+const Form = () => {
+  return (
+    <div>
+      <h1>Estoy en form</h1>
+    </div>
+  );
+};
+
+export default Form;
